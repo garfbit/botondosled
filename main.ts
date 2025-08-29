744d18be6a2e1b10e0588403e161b04ee443697b
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 23:37:07 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.West)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.East)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
